f92ff65323abe902a4e04a32ca20a73bf2f53b88
Rio Fujita <rio_github@rio.st>
2021-11-04 22:22:44 +0900

    Update homebrew-phorganize.rb

--- Formula/homebrew-phorganize.rb
+++ Formula/homebrew-phorganize.rb
@@ -4,8 +4,8 @@
 class HomebrewPhorganize < Formula
   desc "Organize the photos and the videos using embedded meta data in the files"
   homepage ""
-  url "https://github.com/rioriost/homebrew-phorganize/releases/download/0.1/phorganize-v0.1.tgz"
-  sha256 "565482fc27a11d0f82c4ad2d04026242d0e371886f12d6f60a4384e4bf52f718"
+  url "https://github.com/rioriost/homebrew-phorganize/releases/download/0.2/phorganize-v0.2.tgz"
+  sha256 "ae25f151670c3e529e5da04e0758e6916fd5203b146a8be3f3937de1e17db87d"
   license ""
 
   # depends_on "cmake" => :build
